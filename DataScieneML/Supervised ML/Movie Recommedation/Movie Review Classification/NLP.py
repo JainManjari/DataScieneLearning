40c0ce8890807ef29674cf70858dfef8330514c4
Manjari <manjarijain98@gmail.com>
2021-02-17 14:08:14 +0530

    movie review

---
+++ DataScieneML/Supervised ML/Movie Recommedation/Movie Review Classification/NLP.py
@@ -0,0 +1,20 @@
+from nltk.stem.snowball import PorterStemmer
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+
+import sys
+
+ps=PorterStemmer()
+tokenizer=RegexpTokenizer(r'\w+')
+
+
+sw=set(stopwords.words('english'))
+
+
+def mytokenizer(text):
+    text=text.lower()
+    text=text.replace("<br /><br />"," ")
+    words=tokenizer.tokenize(text)
+    words=[w for w in words if w=="not" or w not in sw]
+    words=[ps.stem(w) for w in words]
+    return " ".join(words)
